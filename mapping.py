86d5b2f6fbb762a12d2dcd66c23d7dfa22d68bfe
Joel Matejka <joelm@tejka.cz>
2019-08-10 22:14:39 +0200

    Fix mirrored projection, add orientation in 45deg granularity, formatting

--- mapping.py
+++ mapping.py
@@ -9,14 +9,16 @@ from matplotlib.patches import Arrow
 
 DMAX = 10000
 
+
 def update_arrow():
     global arrow
     arrow.remove()
-    arrow_obj = Arrow(pos_x, pos_y, 0, orientation*1000, width=200, color="red")
+    arrow_obj = Arrow(pos_x, pos_y, 1000 * math.sin(orientation), 1000 * math.cos(orientation), width=200, color="red")
     arrow = ax.add_patch(arrow_obj)
     ax.set_xlim(-DMAX + pos_x, DMAX + pos_x)
     ax.set_ylim(-DMAX + pos_y, DMAX + pos_y)
     plt.draw()
+
 
 def press(event):
     global pos_x
@@ -37,33 +39,36 @@ def press(event):
         pos_x = pos_x + 100
         update_arrow()
     if event.key == 'o':
-        orientation = orientation * -1
+        orientation = (orientation + math.pi / 2) % (2 * math.pi)
         update_arrow()
     if event.key == 'x':
         timestamp, scan = laser.get_filtered_dist(dmax=DMAX)
 
-        cartesian = np.array([[-orientation * distance * math.cos(angle-math.pi/2), -orientation * distance * math.sin(angle-math.pi/2)] for (angle, distance) in scan])
+        cartesian = np.array([[distance * math.cos(angle - orientation - 3 * math.pi / 2),
+                               distance * math.sin(angle - orientation - 3 * math.pi / 2)]
+                               for (angle, distance) in scan])
         cartesian = cartesian + np.array([pos_x, pos_y])
         print(cartesian)
         plot.set_data(*cartesian.T)
         text.set_text('t: %d' % timestamp)
         plt.draw()
 
-#plt.ion()
+
+# plt.ion()
 
 pos_x = 0
 pos_y = 0
 
-orientation = 1
+orientation = 0
 
 laser = HokuyoLX()
 fig, ax = plt.subplots()
 plot = ax.plot([], [], '.')[0]
-arrow_obj = Arrow(pos_x, pos_y, 0, orientation*1000, width=200, color="red")
+arrow_obj = Arrow(pos_x, pos_y, 1000 * math.sin(orientation), 1000 * math.cos(orientation), width=200, color="red")
 arrow = ax.add_patch(arrow_obj)
 text = plt.text(0, 1, '', transform=ax.transAxes)
-ax.set_xlim(-DMAX,DMAX)
-ax.set_ylim(-DMAX,DMAX)
+ax.set_xlim(-DMAX, DMAX)
+ax.set_ylim(-DMAX, DMAX)
 ax.grid(True)
 fig.canvas.mpl_connect('key_press_event', press)
 plt.draw()
